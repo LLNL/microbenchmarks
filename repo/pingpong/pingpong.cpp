8ee54d437463af061b3fd21ba84f7d318e2e9332
stephanielam3211 <lam44@tuolumne2149.llnl.gov>
2026-05-14 21:44:52 -0700

    added debug statements

--- repo/pingpong/pingpong.cpp
+++ repo/pingpong/pingpong.cpp
@@ -823,6 +823,7 @@ int main(int argc, char **argv)
 #if defined(USE_CALIPER)
                 CALI_MARK_BEGIN(warmup_region_aa);
 #endif
+                fprintf("begin warmup");
 
                 for (int i = 0; i < warmup; i++)
                 {
@@ -846,6 +847,7 @@ int main(int argc, char **argv)
                 CALI_MARK_BEGIN(region_label.c_str());
 #endif
 
+                fprintf("end warmup, begin timing");
                 double min_rtt = std::numeric_limits<double>::infinity();
                 double max_rtt = 0.0;
                 int iters = 0;
@@ -893,6 +895,7 @@ int main(int argc, char **argv)
 #if defined(USE_CALIPER)
                 CALI_MARK_END(region_label.c_str());
 #endif
+                fprintf("end timing");
 
 #if defined(USE_HIP)
                 free(aa_send_host);
@@ -908,6 +911,7 @@ int main(int argc, char **argv)
                 free(aa_send);
                 free(aa_recv);
 #endif
+                fprintf("freed memory");
             }
         }
 
